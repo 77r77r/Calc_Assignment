e16db89deebfb978297d38f22326e9d2f68405a5
77r77r <codingworld@naver.com>
2025-08-31 01:37:06 +0900

    ~ t4

--- src/main/java/com/iwon/Calc.java
+++ src/main/java/com/iwon/Calc.java
@@ -1,5 +1,11 @@
-public class Calc {
-    public void run() {
+package com.iwon;
 
+import java.util.Arrays;
+
+public class Calc {
+    public static int run(String args) {
+        String[] arr = args.replace(" ", "")
+                .split("\\+");
+        return Integer.parseInt(arr[0]) + java.lang.Integer.parseInt(arr[1]);
     }
 }
--- src/main/java/com/iwon/Main.java
+++ src/main/java/com/iwon/Main.java
@@ -1,7 +1,7 @@
+package com.iwon;
 
 public class Main {
     public static void main(String[] args) {
-        Calc calc = new Calc();
-        calc.run();
+
     }
 }
