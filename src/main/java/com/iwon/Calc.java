d6bdf965871271e36d5f40769c9cae7241d13849
77r77r <codingworld@naver.com>
2025-08-31 01:47:26 +0900

    t7 minus

--- src/main/java/com/iwon/Calc.java
+++ src/main/java/com/iwon/Calc.java
@@ -1,11 +1,16 @@
 package com.iwon;
 
-import java.util.Arrays;
-
 public class Calc {
     public static int run(String args) {
-        String[] arr = args.replace(" ", "")
-                .split("\\+");
-        return Integer.parseInt(arr[0]) + java.lang.Integer.parseInt(arr[1]);
+        String[] arr = args.split(" ");
+
+        switch(arr[1]) {
+            case "+" :
+                return Integer.parseInt(arr[0]) + java.lang.Integer.parseInt(arr[2]);
+            case "-" :
+                return Integer.parseInt(arr[0]) - java.lang.Integer.parseInt(arr[2]);
+        }
+
+        return 0;
     }
 }
