95ff3e106d6fc315e94f25da2c897f84e7f18c44
77r77r <codingworld@naver.com>
2025-09-02 15:43:13 +0900

    t12

--- src/main/java/com/iwon/Calc.java
+++ src/main/java/com/iwon/Calc.java
@@ -10,32 +10,40 @@ public class Calc {
         System.out.println(Arrays.toString(arr) + ":" + arr.length);
 
         String result = arr[0];
+        int x = 0;
 
-        if (arr.length == 3) {
-            result = operation(result, arr[1], arr[2]);
-            return Integer.parseInt(result);
+        for (int i = 0; i < arr.length - 2 ;) {
+            result = operation(result, arr[i + 1], arr[i + 2]);
+            x = Integer.parseInt(result);
+            System.out.println("result = " + result + ", x=" + x);
+            i += 2;
         }
-        else if (arr.length == 5) {
-            result = operation(result, arr[1], arr[2]);
-            result = operation(result, arr[3], arr[4]);
-
-            return Integer.parseInt(result);
-        }
-        else if (arr.length == 7) {
-            result = operation(result, arr[1], arr[2]);
-            result = operation(result, arr[3], arr[4]);
-            result = operation(result, arr[5], arr[6]);
-
-            return Integer.parseInt(result);
-        }
-        else if (arr.length == 13) {
-            result = operation(result, arr[1], arr[2]);
-            result = operation(result, arr[3], arr[4]);
-            result = operation(result, arr[5], arr[6]);
-            result = operation(result, arr[7], arr[8]);
-
-            return Integer.parseInt(result);
-        }
+//
+//        if (arr.length == 3) {
+//            result = operation(result, arr[1], arr[2]);
+//            return Integer.parseInt(result);
+//        }
+//        else if (arr.length == 5) {
+//            result = operation(result, arr[1], arr[2]);
+//            result = operation(result, arr[3], arr[4]);
+//
+//            return Integer.parseInt(result);
+//        }
+//        else if (arr.length == 7) {
+//            result = operation(result, arr[1], arr[2]);
+//            result = operation(result, arr[3], arr[4]);
+//            result = operation(result, arr[5], arr[6]);
+//
+//            return Integer.parseInt(result);
+//        }
+//        else if (arr.length == 13) {
+//            result = operation(result, arr[1], arr[2]);
+//            result = operation(result, arr[3], arr[4]);
+//            result = operation(result, arr[5], arr[6]);
+//            result = operation(result, arr[7], arr[8]);
+//
+//            return Integer.parseInt(result);
+//        }
 
         return Integer.parseInt(result);
     }
@@ -49,6 +57,8 @@ public class Calc {
                 return String.valueOf(n1 + n2);
             case "-" :
                 return String.valueOf(n1 - n2);
+            case "*" :
+                return String.valueOf(n1 * n2);
         }
 
         return "0";
